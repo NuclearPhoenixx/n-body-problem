988682472f2c25201a9d315ca65c25bd9066f30e
Phoenix1747 <apertured@public-files.de>
2019-06-28 15:33:30 +0200

    also compute potential energy

--- src/gui/system_energy.gd
+++ src/gui/system_energy.gd
@@ -1,6 +1,7 @@
 extends Label
 
 var c = 299792458
+var G = 6.67408e-11 # gravitational constant
 
 func lorentz(v):
 	return 1 / sqrt(1 - pow( v.length()/c ,2))
@@ -12,8 +13,11 @@ func _process(delta):
 		
 		for o in get_tree().get_nodes_in_group("objects"):
 			energy += o.mass * c * sqrt( pow(c,2) + pow(o.v.dot(o.v) * lorentz(o.v), 2))
-			#+ CALCULATE POTENTIAL ENERGY. ONLY KINETIC AND REST ENERGY RIGHT NOW
+			for a in get_tree().get_nodes_in_group("objects"):
+				if a != o:
+					var r = a.position - o.position
+					energy += G * a.mass * o.mass / r.length()
 		
-		text = "Total System Energy = " + str(stepify(energy/1e30,1)) + " [ 1e30 J ]"
+		text = "Total System Energy = " + str(stepify(energy/1e40,1)) + " [ 1e40 J ]"
 	else:
 		hide()
