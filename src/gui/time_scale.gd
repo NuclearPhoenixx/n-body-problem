ef073ea1bc7d1975899b8022d08a833aab8cba14
Phoenix1747 <apertured@public-files.de>
2019-11-13 19:25:44 +0100

    change timescale

--- src/gui/time_scale.gd
+++ src/gui/time_scale.gd
@@ -1,7 +1,7 @@
 extends Label
 
 func _on_TimeScale_value_changed(value):
-	var scale = pow(20, value)
+	var scale = pow(10, value)
 	
 	text = "Timescale: " + str(stepify(scale, .001))
 	Engine.set_time_scale(scale) #adjust the engine clock tick
